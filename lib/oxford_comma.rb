e3e9b37fd339c134e2517f9b9f63ccbd9644b862
Rico Mitchell <ricomitch@icloud.com>
2019-10-17 03:18:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  end
+#binding.pry
 end
