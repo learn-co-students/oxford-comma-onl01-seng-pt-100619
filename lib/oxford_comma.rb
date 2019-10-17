729b5b881bfb2e5460d3bf670651aeb47a19517f
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-17 01:35:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  new_array = []
+  if array.length == 1
+    return "#{array[0]}"
+elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    #element = array[-1].insert(0, "and ")
+    #array.unshift << element
+    #array.uniq.join(", ")
+    last_element = array.pop
+    array.join(", ") << ", and " << last_element
+  end
+
+#binding.pry
 end
