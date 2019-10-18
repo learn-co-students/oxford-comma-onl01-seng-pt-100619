ce52df268d7843e054cfcb1267e6d4aa58c7289f
Abraham Moreno <adiaz2821@gmail.com>
2019-10-18 15:34:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
-
-end
+def oxford_comma(list)
+if list.length == 1
+  return list.join
+elsif list.length == 2
+  return list.join(" and ")
+elsif list.length >= 3
+  list.last.insert(0, "and ")
+  return list.join(", ")
+else 
+  return "this"
+end 
+end 
