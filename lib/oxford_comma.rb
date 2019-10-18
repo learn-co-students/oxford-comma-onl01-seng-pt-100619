d6064c0e880a511b3d168a26213c4d233ee9825d
Eric Manning <emanningmusic@yahoo.com>
2019-10-18 20:26:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def oxford_comma(array)
 
+  if array.size == 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    #binding.pry
+    array[-1] = "and " + array[-1]
+    
+    
+  end
+array.join(", ")
 end
